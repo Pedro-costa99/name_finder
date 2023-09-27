a037a4ee827f8e3c970c5e6162f2e7ec5da260de
Pedro Canabrava <pedro-ccosta@live.com>
2023-09-27 19:03:12 -0300

    Identica arquivo index, limpa codigo css

--- src/pages/SVGBackground.tsx
+++ src/pages/SVGBackground.tsx
@@ -3,33 +3,11 @@ import React from "react";
 const SVGBackground = () => {
 
   return (
-    //   <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 1920 1080" preserveAspectRatio="none">
-    //   <polygon points="0,0 1920,0 1920,540" fill="#FFB6C1" />
-    //   <polygon points="0,1080 1920,1080 1920,540" fill="#ADD8E6" />
-    //   <path
-    //     d="M0,540 C180,600 360,540 540,540 C720,540 900,600 1080,540 C1260,480 1440,540 1620,540 C1800,540 1920,480 1920,540"
-    //     fill="#FFF"
-    //   />
-    // </svg>
-//     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 1920 1080" preserveAspectRatio="none">
-//   <polygon points="0,0 960,0 960,540" fill="#FFB6C1" />
-//   <polygon points="0,1080 960,540 1920,1080" fill="#ADD8E6" />
-//   <path d="M960,0 C1140,60 1320,0 1440,0 C1560,0 1680,60 1800,0" fill="#FFF" />
-//   <path d="M960,1080 C780,1020 600,1080 480,1080 C360,1080 240,1020 120,1080" fill="#FFF" />
-// </svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="1920" height="1080" viewBox="0 0 1920 1080" preserveAspectRatio="none">
-  <rect x="0" y="0" width="960" height="1080" fill="#FFB6C1" />
-  <rect x="960" y="0" width="960" height="1080" fill="#ADD8E6" />
-</svg>
-
-
-
-
-
-
-
-
-
+  <svg xmlns="http://www.w3.org/2000/svg" width="1920" height="1080" viewBox="0 0 1920 1080" preserveAspectRatio="none">
+    <rect x="0" y="0" width="960" height="1080" fill="#FFB6C1" />
+    <rect x="960" y="0" width="960" height="1080" fill="#ADD8E6" />
+    <path d="M960 0 Q 720 270, 960 540 Q 1200 810, 960 1080" fill="transparent" stroke="#FFF" strokeWidth="4" />
+  </svg>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,157 +1,161 @@
-import { Button, Chip, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent } from '@mui/material';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {
+  Button,
+  Chip,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
 import './styles.css';
 import SVGBackground from './SVGBackground.tsx';
 import { renderToString } from 'react-dom/server';
 
-
-
 interface DataItem {
-  names: string[]
+  names: string[];
 }
 
-const Home = () =>{
-
+const Home = () => {
   const svgString = renderToString(<SVGBackground />);
-    const [usage, setUsage] = useState<string>('');
-    const [data, setData] = useState<DataItem | null>(null)
-    const [gender, setGender] = useState('')
-    const [number, setNumber] = useState('')
-    const key = "pe839753942"
+  const [usage, setUsage] = useState<string>('');
+  const [data, setData] = useState<DataItem | null>(null);
+  const [gender, setGender] = useState('');
+  const [number, setNumber] = useState('');
+  const key = 'pe839753942';
 
-    const namesSelect = {
-      'eng': 'Inglês',
-      'ita': 'Italiano',
-      'fre': 'Francês',
-      'por': 'Português',
-      'afr': 'Africano',
-      'spa': 'Espanhol'
-    };
+  const namesSelect = {
+    eng: 'Inglês',
+    ita: 'Italiano',
+    fre: 'Francês',
+    por: 'Português',
+    afr: 'Africano',
+    spa: 'Espanhol',
+  };
 
-      const handleSearchClick = () => {
-        fetch(`https://www.behindthename.com/api/random.json?number=${number}&usage=${usage}&gender=${gender}&key=${key}`)
-          .then(response => response.json())
-          .then(jsonData => setData(jsonData))
-          .catch(error => console.log(error));
-      }
+  const handleSearchClick = () => {
+    fetch(`https://www.behindthename.com/api/random.json?number=${number}&usage=${usage}&gender=${gender}&key=${key}`)
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.log(error));
+  };
 
-      const handleGenderChange = (value: string) => {
-        setGender(value);
-      };
-    
-      const handleNumberChange = (value: string) => {
-        setNumber(value);
-      };
+  const handleGenderChange = (value: string) => {
+    setGender(value);
+  };
 
-      const handleChange = (event: SelectChangeEvent<typeof usage>) => {
-        const {
-          target: { value },
-        } = event;
-        setUsage(value);
-      };
+  const handleNumberChange = (value: string) => {
+    setNumber(value);
+  };
 
-      const ITEM_HEIGHT = 24;
-      const ITEM_PADDING_TOP = 8;
+  const handleChange = (event: SelectChangeEvent<typeof usage>) => {
+    const {
+      target: { value },
+    } = event;
+    setUsage(value);
+  };
 
-      const MenuProps = {
-        PaperProps: {
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-          },
-        },
-      };
+  const ITEM_HEIGHT = 24;
+  const ITEM_PADDING_TOP = 8;
 
-      const genders = [
-        { label: 'Female', value: 'F' },
-        { label: 'Male', value: 'M' },
-      ];
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
 
-      const isSearchDisabled = !(gender && number)
+  const genders = [
+    { label: 'Female', value: 'F' },
+    { label: 'Male', value: 'M' },
+  ];
 
-      
-    
-      return (
-      
-                 <Grid container
-                  direction="column"
-                  justifyContent="center"
-                  alignItems="center"
-                  spacing={2}
-                  sx={{ minHeight: '100vh',  backgroundImage: `url("data:image/svg+xml;utf8,${encodeURIComponent(svgString)}")`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center'}}
+  const isSearchDisabled = !(gender && number);
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      sx={{
+        minHeight: '100vh',
+        backgroundImage: `url("data:image/svg+xml;utf8,${encodeURIComponent(svgString)}")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Grid sx={{ p: 2, textAlign: 'center' }}>
+        <h1>Encontre o nome perfeito para seu bebê...</h1>
+      </Grid>
+      <Grid container sx={{ p: 2 }}>
+        <Grid item md={6} container justifyContent="center" sx={{ mb: 2 }}>
+          {genders.map((genderOption) => (
+            <Chip
+              key={genderOption.value}
+              label={genderOption.label}
+              color={gender === genderOption.value ? 'primary' : 'default'}
+              onClick={() => handleGenderChange(genderOption.value)}
+              sx={{ m: 1 }}
+            />
+          ))}
+        </Grid>
+        <Grid item md={6} container justifyContent="center" sx={{ mb: 2 }}>
+          {[1, 2, 3, 4, 5, 6].map((value) => (
+            <Chip
+              key={value}
+              label={String(value)}
+              color={number === String(value) ? 'primary' : 'default'}
+              onClick={() => handleNumberChange(String(value))}
+              sx={{ m: 1 }}
+            />
+          ))}
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center">
+        <Grid item xs={10} sm={4} md={4} sx={{ p: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-multiple-name-label">Name</InputLabel>
+            <Select
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              value={usage}
+              onChange={handleChange}
+              input={<OutlinedInput label="Name" />}
+              MenuProps={MenuProps}
             >
-                  <Grid  sx={{ p: 2, textAlign: 'center' }}>
-                    <h1>Encontre o nome perfeito para seu bebê...</h1>
-                  </Grid>
-                  <Grid container sx={{ p: 2 }}>
-                  <Grid item md={6} container justifyContent="center" sx={{ mb: 2 }}>
-                  {genders.map((genderOption) => (
-                    <Chip
-                      key={genderOption.value}
-                      label={genderOption.label}
-                      color={gender === genderOption.value ? 'primary' : 'default'}
-                      onClick={() => handleGenderChange(genderOption.value)}
-                      sx={{ m: 1 }}
-                    />
-                  ))}
-                </Grid>
-                      <Grid item md={6} container justifyContent="center" sx={{ mb: 2 }}>
-                      {[1, 2, 3, 4, 5, 6].map((value) => (
-                        <Chip
-                          key={value}
-                          label={String(value)}
-                          color={number === String(value) ? 'primary' : 'default'}
-                          onClick={() => handleNumberChange(String(value))}
-                          sx={{ m: 1 }}
-                        />
-                      ))}
-                    </Grid>
-                  </Grid>
-                  <Grid container justifyContent="center">
-                    <Grid item xs={10}  sm={4} md={4} sx={{ p: 2}}>
-                      <FormControl fullWidth
-                      >
-                        <InputLabel id="demo-multiple-name-label">Name</InputLabel>
-                        <Select
-                          labelId="demo-multiple-name-label"
-                          id="demo-multiple-name"
-                          value={usage}
-                          onChange={handleChange}
-                          input={<OutlinedInput label="Name" />}
-                          MenuProps={MenuProps}
-                        >
-                          {Object.keys(namesSelect).map((name) => (
-                            <MenuItem 
-                              key={name}
-                              value={name}
-                            >
-                              {namesSelect[name]}
-                            </MenuItem>
-                          ))}
-                        </Select>
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                  <Grid item sx={{ p: 3 }}>
-                    <Button variant="outlined" onClick={handleSearchClick} disabled={isSearchDisabled}>Pesquisar</Button>
-                  </Grid>
-                  <Grid item sx={{ p: 3, textAlign: 'center'  }}>
-                  {data && data?.names && data?.names.length > 0 ?(
-                        data?.names.map((name) => (
-                            <div key={name} className="magic-name">
-                              <span >{name}</span>
-                            </div>
-                        ))
-                  ):
-                  <span>Data not found</span>
-                  }
-                  </Grid>
-              </Grid>
-      
-
-      );
-}
+              {Object.keys(namesSelect).map((name) => (
+                <MenuItem key={name} value={name}>
+                  {namesSelect[name]}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid item sx={{ p: 3 }}>
+        <Button variant="outlined" onClick={handleSearchClick} disabled={isSearchDisabled}>
+          Pesquisar
+        </Button>
+      </Grid>
+      <Grid item sx={{ p: 3, textAlign: 'center' }}>
+        {data && data?.names && data?.names.length > 0 ? (
+          data?.names.map((name) => (
+            <div key={name} className="magic-name">
+              <span>{name}</span>
+            </div>
+          ))
+        ) : (
+          <span>Data not found</span>
+        )}
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Home
